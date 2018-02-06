74dc7b968b6609508e590695bf751c6ea1d53eff
HarlanLuo <luoxwen@gmail.com>
2018-02-06 17:29:17 +0800

    添加sudo-su插件

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -34,6 +34,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (config('app.debug')) {
+            $this->app->register('VIACreative\SudoSu\ServiceProvider');
+        }
     }
 }
